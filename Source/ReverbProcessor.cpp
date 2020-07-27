ca58df50fc41b72533f6bc4421320b836872b916
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2020-07-27 16:20:38 +0100

    Create ReverbProcessor class

---
+++ Source/ReverbProcessor.cpp
@@ -0,0 +1,21 @@
+/**
+* \class Toolkit3dtiProcessor
+*
+* \brief Declaration of Toolkit3dtiProcessor interface.
+* \date  June 2019
+*
+* \authors Reactify Music LLP: R. Hrafnkelsson ||
+* Coordinated by , A. Reyes-Lecuona (University of Malaga) and L.Picinali (Imperial College London) ||
+* \b Contact: areyes@uma.es and l.picinali@imperial.ac.uk
+*
+* \b Project: 3DTI (3D-games for TUNing and lEarnINg about hearing aids) ||
+* \b Website: http://3d-tune-in.eu/
+*
+* \b Copyright: University of Malaga and Imperial College London - 2019
+*
+* \b Licence: This copy of the 3D Tune-In Toolkit Plugin is licensed to you under the terms described in the LICENSE.md file included in this distribution.
+*
+* \b Acknowledgement: This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreements No 644051 and 726765.
+*/
+
+#include "ReverbProcessor.h"
